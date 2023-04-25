3f722593e8df3f492f3c50785d60bde3d566cdd9
comghost <overpassion@gmail.com>
2023-04-25 15:30:05 +0900

    commit

---
+++ delivery/src/main/java/kafka/scaling/domain/Delivery.java
@@ -0,0 +1,131 @@
+package kafka.scaling.domain;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+import kafka.scaling.DeliveryApplication;
+import kafka.scaling.domain.DeliveryCancelled;
+import kafka.scaling.domain.DeliveryModified;
+import kafka.scaling.domain.DeliveryStarted;
+import lombok.Data;
+
+@Entity
+@Table(name = "Delivery_table")
+@Data
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long orderId;
+
+    private Long customerId;
+
+    private String productId;
+
+    private String productName;
+
+    private Integer qty;
+
+    private String address;
+
+    private String status;
+
+    @PostPersist
+    public void onPostPersist() {
+        DeliveryStarted deliveryStarted = new DeliveryStarted(this);
+        deliveryStarted.publishAfterCommit();
+    }
+
+    @PostUpdate
+    public void onPostUpdate() {
+        DeliveryModified deliveryModified = new DeliveryModified(this);
+        deliveryModified.publishAfterCommit();
+    }
+
+    @PreRemove
+    public void onPreRemove() {
+        DeliveryCancelled deliveryCancelled = new DeliveryCancelled(this);
+        deliveryCancelled.publishAfterCommit();
+    }
+
+    public static DeliveryRepository repository() {
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(
+            DeliveryRepository.class
+        );
+        return deliveryRepository;
+    }
+
+    public static void startDelivery(OrderPlaced orderPlaced) {
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
+        deliveryStarted.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderPlaced.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+            DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
+            deliveryStarted.publishAfterCommit();
+
+         });
+        */
+
+    }
+
+    public static void modifyDelivery(OrderModified orderModified) {
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        DeliveryModified deliveryModified = new DeliveryModified(delivery);
+        deliveryModified.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderModified.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+            DeliveryModified deliveryModified = new DeliveryModified(delivery);
+            deliveryModified.publishAfterCommit();
+
+         });
+        */
+
+    }
+
+    public static void cancelDelivery(OrderCancelled orderCancelled) {
+        /** Example 1:  new item 
+        Delivery delivery = new Delivery();
+        repository().save(delivery);
+
+        DeliveryCancelled deliveryCancelled = new DeliveryCancelled(delivery);
+        deliveryCancelled.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+        repository().findById(orderCancelled.get???()).ifPresent(delivery->{
+            
+            delivery // do something
+            repository().save(delivery);
+
+            DeliveryCancelled deliveryCancelled = new DeliveryCancelled(delivery);
+            deliveryCancelled.publishAfterCommit();
+
+         });
+        */
+
+    }
+}
---
+++ delivery/src/main/java/kafka/scaling/domain/DeliveryRepository.java
@@ -0,0 +1,12 @@
+package kafka.scaling.domain;
+
+import kafka.scaling.domain.*;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(
+    collectionResourceRel = "deliveries",
+    path = "deliveries"
+)
+public interface DeliveryRepository
+    extends PagingAndSortingRepository<Delivery, Long> {}
